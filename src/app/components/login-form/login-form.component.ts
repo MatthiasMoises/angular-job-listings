8b60826fc307374ea59fa50b7a3404122eb1a502
Matthias Moises <mail@matthias-moises.de>
2025-01-20 17:07:13 +0100

    Changed Login Form to Template driven form

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,44 +1,44 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { LoggerService, LogLevel } from '../../services/logger.service';
 import { Router } from '@angular/router';
-import { NgClass, NgIf } from '@angular/common';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [ReactiveFormsModule, NgIf, NgClass],
+  imports: [FormsModule, NgClass],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
 export class LoginFormComponent {
-  userForm = new FormGroup({
-    email: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required)
-  })
+  model = {
+    email: '',
+    password: ''
+  }
+
+  loginError: string | null = null
+
   private readonly authService = inject(AuthService)
   private readonly loggerService = inject(LoggerService)
   private readonly router = inject(Router)
 
-  get email() {
-    return this.userForm.get('email')
-  }
-
-  get password() {
-    return this.userForm.get('password')
-  }
-
-  async onSubmit() {
+  async onSubmit(loginForm: NgForm) {
     const authUser: { email: string, password: string } = {
-      email: this.userForm.getRawValue().email as string,
-      password: this.userForm.getRawValue().password as string
+      email: this.model.email as string,
+      password: this.model.password as string
     }
 
     try {
-      const authResult = await this.authService.login(authUser)
-      if (authResult) {
+      const authSuccess = await this.authService.login(authUser)
+
+      if (authSuccess) {
         this.router.navigate(['/'])
+      }
+      else {
+        loginForm.resetForm()
+        this.loginError = 'Invalid credentials. Please try again'
       }
     } catch (error) {
       this.loggerService.writeToLog(error as string, LogLevel.ERROR)
