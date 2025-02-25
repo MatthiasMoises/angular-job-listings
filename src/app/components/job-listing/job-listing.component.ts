944aca4da366de4551f1e2a7b99ffd262c0aa6ac
Matthias Moises <mail@matthias-moises.de>
2025-02-25 17:34:06 +0100

    Added Angular Transitions for job description text

--- src/app/components/job-listing/job-listing.component.ts
+++ src/app/components/job-listing/job-listing.component.ts
@@ -4,13 +4,32 @@ import { RouterModule } from '@angular/router';
 import { Job } from '../../interfaces/job';
 import { DatePipe } from '@angular/common';
 import { HighlightDirective } from '../../directives/highlight.directive';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-job-listing',
   standalone: true,
   imports: [RouterModule, NgIconsModule, DatePipe, HighlightDirective],
   templateUrl: './job-listing.component.html',
-  styleUrl: './job-listing.component.css'
+  styleUrl: './job-listing.component.css',
+  animations: [
+    trigger('toggleDescription', [
+      state(
+        'more',
+        style({
+          height: '110px',
+        }),
+      ),
+      state(
+        'less',
+        style({
+          height: '50px',
+        }),
+      ),
+      transition('more => less', [animate('0.1s')]),
+      transition('less => more', [animate('0.1s')]),
+    ]),
+  ]
 })
 export class JobListingComponent implements OnInit {
   job = input.required<Job>()
